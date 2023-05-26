418361ce3ed9ee3ce7efecdc9a539f704c5785bc
Tommy Green <tommygreen@Tommys-Huawei-MateBook-14s.local>
2023-05-26 18:15:58 +0800

    fix: fix page isze

--- search_example/search_api/paginators.py
+++ search_example/search_api/paginators.py
@@ -2,9 +2,9 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 20
     page_size_query_param = 'page_size'
-    max_page_size = 20
+    max_page_size = 100
     page_query_param = 'page'
 
     def get_page_number(self, request, paginator):
@@ -12,3 +12,10 @@ class CustomPagination(PageNumberPagination):
         if page_number in self.last_page_strings:
             page_number = paginator.num_pages
         return page_number
+
+    def get_page_size(self, request):
+        page_size = request.data.get(self.page_size_query_param, self.page_size)
+        if page_size > 0:
+            return page_size
+
+        return self.page_size
